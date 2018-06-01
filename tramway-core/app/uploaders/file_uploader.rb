10415a72fedf65fe4bc6dec531846c433397b284
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-06-01 12:40:22 +0400

    #core #user fixes

--- tramway-core/app/uploaders/file_uploader.rb
+++ tramway-core/app/uploaders/file_uploader.rb
@@ -1,5 +1,5 @@
 class FileUploader < ApplicationUploader
   def extension_white_list
-    %w(pdf doc docx xls csv xlsx)
+    %w(pdf doc docx xls csv xlsx jpg)
   end
 end
--- tramway-user/app/controllers/tramway/user/web/application_controller.rb
+++ tramway-user/app/controllers/tramway/user/web/application_controller.rb
@@ -1,7 +1,5 @@
 require_dependency "tramway/user/application_controller"
 
-module Tramway::User
-  class Web::ApplicationController < ApplicationController
-    include AuthManagment
-  end
+class Tramway::User::Web::ApplicationController < ::Tramway::User::ApplicationController
+  include AuthManagment
 end
