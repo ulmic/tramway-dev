6075dd421cd5d85b176dd1b954993650bb445277
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-06-24 00:19:02 +0400

    #landing add page to block link

--- tramway-landing/app/models/tramway/landing/block.rb
+++ tramway-landing/app/models/tramway/landing/block.rb
@@ -1,7 +1,7 @@
 class Tramway::Landing::Block < ::Tramway::Landing::ApplicationRecord
   enumerize :block_type, in: [ :header, :footer, :page, :cards, :features, :contacts, :news, :link ]
   enumerize :navbar_link, in: [ :exist, :not_exist ], default: :not_exist
-  enumerize :link_object_type, in: [ 'Tramway::SportSchool::Document' ]
+  enumerize :link_object_type, in: [ 'Tramway::SportSchool::Document', 'Tramway::Page::Page' ]
 
   mount_uploader :background, PhotoUploader
 
