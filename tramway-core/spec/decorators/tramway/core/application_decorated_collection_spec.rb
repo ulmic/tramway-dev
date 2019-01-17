7717e2f7b875051f78932d2d5b8f294e12dcec68
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-01-18 00:35:18 +0400

    #core add more application_decorated_collection_spec

--- tramway-core/spec/decorators/tramway/core/application_decorated_collection_spec.rb
+++ tramway-core/spec/decorators/tramway/core/application_decorated_collection_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
+require 'kaminari'
 
 RSpec.describe Tramway::Core::ApplicationDecoratedCollection do
   it 'defined decorator class' do
     expect(defined?(described_class)).to be_truthy
   end
 
-#  it 'should initialize new decorated array' do
-#    arr = (1..10).to_a.map do |index|
-#     "it can be any object #{index} times"
-#    end
-#    expect{ described_class.new arr }.not_to raise_error(StandardError)
-#  end
+  it 'should initialize new decorated array' do
+    create_list(:test_model, 20)
+    test_models = TestModel.all.page(5)
+    decorated_test_models = test_models.map { |t| Tramway::Core::ApplicationDecorator.new t }
+    expect{ described_class.new decorated_test_models, test_models }.not_to raise_error(StandardError)
+  end
 end
--- tramway-core/tramway-core.gemspec
+++ tramway-core/tramway-core.gemspec
@@ -28,6 +28,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'haml-rails', '~> 1.0', '>= 1.0.0'
   s.add_dependency 'font-awesome-rails'
   s.add_dependency 'carrierwave'
+  s.add_dependency 'kaminari'
 
   s.add_development_dependency 'rspec-rails'
   s.add_development_dependency 'pry'
