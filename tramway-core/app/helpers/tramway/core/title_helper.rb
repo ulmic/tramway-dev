c178b23ebaad639f69681b27968883bae974945c
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-02-11 22:41:31 +0400

    #core add one more error message

--- tramway-core/app/helpers/tramway/core/title_helper.rb
+++ tramway-core/app/helpers/tramway/core/title_helper.rb
@@ -2,8 +2,13 @@ module Tramway
   module Core
     module TitleHelper
       def title(page_title = default_title)
-        title_text = "#{page_title} | #{@application.title}"
-        content_for(:title) { title_text }
+        if @application.present?
+          title_text = "#{page_title} | #{@application.title}" 
+          content_for(:title) { title_text }
+        else
+          error = Tramway::Error.new(plugin: :core, method: :title, message: ('You should set Tramway::Core::Application class using `::Tramway::Core.initialize_application model_class: #{model_class_name}` in config/initializers/tramway.rb'))
+          raise error.message
+        end
       end
 
       def default_title
