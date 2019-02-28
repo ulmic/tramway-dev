bef7372eaaa9237b06fa58d9f9e38b3c76c6807a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-03-01 03:43:40 +0400

    #api add api/records

--- tramway-api/app/controllers/tramway/api/v1/records_controller.rb
+++ tramway-api/app/controllers/tramway/api/v1/records_controller.rb
@@ -7,5 +7,34 @@ module Tramway::Api::V1
         each_serializer: serializer,
         status: :ok
     end
+
+    def create
+      record_form = form_class.new model_class.new
+      if record_form.submit params[:data][:attributes]
+        render json: company_form.model,
+          serializer: serializer_class,
+          status: :created
+      else
+        render_errors_for record_form
+      end
+    end
+
+    private
+
+    def model_class
+      params[:model].constantize
+    end
+
+    def decorator_class(model_name = nil)
+      "#{model_name || model_class}Decorator".constantize
+    end
+
+    def form_class(model_name = nil)
+      "#{model_name || model_class}Form".constantize
+    end
+
+    def serializer_class(model_name = nil)
+      "#{model_name || model_class}Serializer".constantize
+    end
   end
 end
--- tramway-api/config/routes.rb
+++ tramway-api/config/routes.rb
@@ -2,5 +2,6 @@ Tramway::Api::Engine.routes.draw do
   namespace :v1 do
     resource :user_token, only: [ :create ]
     resource :user, only: [ :create, :show ]
+    resources :records
   end
 end
