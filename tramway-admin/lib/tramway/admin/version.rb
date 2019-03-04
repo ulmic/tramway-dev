eb9c6f4d4d99c30dd8ea8af60e6809226af7e08a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-03-05 02:27:25 +0400

    #admin #user update versions

--- tramway-admin/lib/tramway/admin/version.rb
+++ tramway-admin/lib/tramway/admin/version.rb
@@ -1,5 +1,5 @@
 module Tramway
   module Admin
-    VERSION = '1.8.1.2'
+    VERSION = '1.8.1.3'
   end
 end
--- tramway-user/lib/tramway/user/version.rb
+++ tramway-user/lib/tramway/user/version.rb
@@ -1,5 +1,5 @@
 module Tramway
   module User
-    VERSION = '1.1.6'
+    VERSION = '1.1.7'
   end
 end
