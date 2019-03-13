310df51374d3c6b2ab0d758f8f72187a65332691
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-03-13 21:22:30 +0400

    #admin add search

--- tramway-admin/app/controllers/tramway/admin/records_controller.rb
+++ tramway-admin/app/controllers/tramway/admin/records_controller.rb
@@ -1,7 +1,9 @@
 module Tramway::Admin
   class RecordsController < ApplicationController
     def index
-      @records = decorator_class.decorate model_class.active.order(id: :desc).send(params[:scope] || :all).page params[:page]
+      records = model_class.active.order(id: :desc).send params[:scope] || :all
+      records.search params[:search] if params[:search]
+      @records = decorator_class.decorate records.page params[:page]
     end
 
     def show
