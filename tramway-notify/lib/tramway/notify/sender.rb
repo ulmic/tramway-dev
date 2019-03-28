5c2cf06f744d27e532dd23c554873c7357563971
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-03-28 04:06:05 +0400

    #notify add sender

--- tramway-notify/lib/tramway/notify/sender.rb
+++ tramway-notify/lib/tramway/notify/sender.rb
@@ -2,8 +2,8 @@ module Tramway::Notify::Sender
   class << self
     def send_now
       ::Tramway::Notify::Notification.active.ready.find_each do |notification|
-        if notification.departure_date < DateTime.now
-          "#{notification.notification_type}Service".constantize.send_notification
+        if notification.departure_date.nil? || notification.departure_date < DateTime.now
+          "#{notification.notification_type.capitalize}Service".constantize.send_notification
         end
       end
     end
