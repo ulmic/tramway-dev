1cd0283aebf27262396bab39d4ba8775d10dfbb4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-04-04 03:42:15 +0400

    #api add error with message

--- tramway-api/app/controllers/tramway/api/v1/application_controller.rb
+++ tramway-api/app/controllers/tramway/api/v1/application_controller.rb
@@ -5,6 +5,10 @@ module Tramway
         def render_errors_for(model)
           render json: model, status: :unprocessable_entity, serializer: ::Tramway::Api::V1::ErrorSerializer
         end
+
+        def render_error_with_text(text)
+          render json: { text: text }, status: :bad_request
+        end
       end
     end
   end
