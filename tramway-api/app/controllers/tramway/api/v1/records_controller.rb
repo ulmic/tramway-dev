4f4534440dcc20dca677b6c37c192375e201491f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-05-01 20:56:03 +0400

    #api fix bug

--- tramway-api/app/controllers/tramway/api/v1/records_controller.rb
+++ tramway-api/app/controllers/tramway/api/v1/records_controller.rb
@@ -8,7 +8,7 @@ module Tramway::Api::V1
     end
 
     def create
-      record_form = form_class.new
+      record_form = form_class.new model_class.new
       if record_form.submit params[:data][:attributes]
         render json: record_form.model,
           serializer: serializer_class,
