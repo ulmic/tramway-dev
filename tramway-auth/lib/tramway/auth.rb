4039a15693570ab200867af6b3961e9762779a37
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-05-12 01:31:10 +0400

    # auth some udpates

--- tramway-auth/lib/tramway/auth.rb
+++ tramway-auth/lib/tramway/auth.rb
@@ -3,6 +3,19 @@ require "tramway/auth/engine"
 module Tramway
   module Auth
     class << self
+      def authenticable_classes
+        @authenticable_classes ||= []
+      end
+
+      def authenticable_classes=(value)
+        @authenticable_classes ||= []
+        if value.is_a? Array
+          @authenticable_classes += value
+        else
+          @authenticable_classes << value
+        end
+      end
+      
       def root
         File.dirname __dir__
       end
