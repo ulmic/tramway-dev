57ae282825ceef336a4a150c50b07818f653dd85
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-08-05 04:10:48 +0400

    #core #event errors view

--- tramway-event/app/controllers/tramway/event/participants_controller.rb
+++ tramway-event/app/controllers/tramway/event/participants_controller.rb
@@ -5,7 +5,7 @@ class Tramway::Event::ParticipantsController < Tramway::Event::ApplicationContro
     if @participant_form.submit params[:tramway_event_participant].except :event_id
       redirect_to event_path event, flash: :success
     else
-      redirect_to event_path event, flash: :error
+      redirect_to event_path event, flash: :error, errors: @participant_form.errors.messages
     end
   end
 end
