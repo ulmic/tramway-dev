43b8111ae9276beadedeed189c1986ce65e073f7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-11-12 03:23:40 +0400

    #core form_properties_additional feature

--- tramway-core/app/forms/tramway/core/application_form.rb
+++ tramway-core/app/forms/tramway/core/application_form.rb
@@ -58,11 +58,17 @@ module Tramway::Core
       @form_properties = args
     end
 
+    def form_properties_additional(**args)
+      @form_properties_additional = args
+    end
+
     def properties
       return @form_properties if @form_properties
       yaml_config_file_path = Rails.root.join('app', 'forms', "#{self.class.name.underscore}.yml")
       if File.exist? yaml_config_file_path
         @form_properties = YAML.load_file(yaml_config_file_path).deep_symbolize_keys
+        @form_properties.deep_merge! @form_properties_additional if @form_properties_additional
+        @form_properties
       else
         []
       end
--- tramway-core/lib/tramway/core/version.rb
+++ tramway-core/lib/tramway/core/version.rb
@@ -2,6 +2,6 @@
 
 module Tramway
   module Core
-    VERSION = '1.14.1.2'
+    VERSION = '1.14.2'
   end
 end
