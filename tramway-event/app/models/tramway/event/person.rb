f3e65d864062a4e604f9ee74127c1dd4ab2ffee4
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-06 19:01:20 +0400

    #event fix bug with associations

--- tramway-event/app/models/tramway/event/person.rb
+++ tramway-event/app/models/tramway/event/person.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class Tramway::Event::Person < ::Tramway::Event::ApplicationRecord
-  has_and_belongs_to_many :sections, class_name: 'Tramway::Event::Section'
-
   mount_uploader :photo, PhotoUploader
 end
--- tramway-event/lib/tramway/event/version.rb
+++ tramway-event/lib/tramway/event/version.rb
@@ -2,6 +2,6 @@
 
 module Tramway
   module Event
-    VERSION = '1.9.19.4'
+    VERSION = '1.9.19.5'
   end
 end
