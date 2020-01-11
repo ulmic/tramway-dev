d1c51a2098d211a20a5fb7e3efa99cf9b8df58a8
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-11 17:31:48 +0400

    #core fix bug with new image_view decorator method

--- tramway-core/app/decorators/tramway/core/concerns/attributes_decorator_helper.rb
+++ tramway-core/app/decorators/tramway/core/concerns/attributes_decorator_helper.rb
@@ -14,30 +14,32 @@ module Tramway::Core::Concerns::AttributesDecoratorHelper
   end
 
   def image_view(original, thumb: nil, filename: nil)
-    thumb ||= original.small
-    filename ||= original.path.split('/').last
-    src_thumb = if thumb.is_a?(CarrierWave::Uploader::Base)
-                  thumb.url
-                elsif thumb.match(%r{^(?:[a-zA-Z0-9+/]{4})*(?:|(?:[a-zA-Z0-9+/]{3}=)|(?:[a-zA-Z0-9+/]{2}==)|(?:[a-zA-Z0-9+/]{1}===))$})
-                  "data:image/jpeg;base64,#{thumb}"
-                else
-                  thumb
-                end
-    src_original = if original.is_a?(CarrierWave::Uploader::Base)
-                  original.url
-                elsif original.match(%r{^(?:[a-zA-Z0-9+/]{4})*(?:|(?:[a-zA-Z0-9+/]{3}=)|(?:[a-zA-Z0-9+/]{2}==)|(?:[a-zA-Z0-9+/]{1}===))$})
-                  "data:image/jpeg;base64,#{original}"
-                else
-                  original
-                end
-    content_tag(:div) do
-      begin
-        concat image_tag src_thumb
-      rescue NoMethodError => e
-        error = Tramway::Error.new plugin: :core, method: :image_view, message: "You should mount PhotoUploader to your model. Just add `mount_uploader \#{attribute_name}, PhotoUploader` to your model. #{e.message}"
-        raise error.message
+    if original.present?
+      thumb ||= original.small
+      filename ||= original.path.split('/').last
+      src_thumb = if thumb.is_a?(CarrierWave::Uploader::Base)
+                    thumb.url
+                  elsif thumb.match(%r{^(?:[a-zA-Z0-9+/]{4})*(?:|(?:[a-zA-Z0-9+/]{3}=)|(?:[a-zA-Z0-9+/]{2}==)|(?:[a-zA-Z0-9+/]{1}===))$})
+                    "data:image/jpeg;base64,#{thumb}"
+                  else
+                    thumb
+                  end
+      src_original = if original.is_a?(CarrierWave::Uploader::Base)
+                       original.url
+                     elsif original.match(%r{^(?:[a-zA-Z0-9+/]{4})*(?:|(?:[a-zA-Z0-9+/]{3}=)|(?:[a-zA-Z0-9+/]{2}==)|(?:[a-zA-Z0-9+/]{1}===))$})
+                       "data:image/jpeg;base64,#{original}"
+                     else
+                       original
+                     end
+      content_tag(:div) do
+        begin
+          concat image_tag src_thumb
+        rescue NoMethodError => e
+          error = Tramway::Error.new plugin: :core, method: :image_view, message: "You should mount PhotoUploader to your model. Just add `mount_uploader \#{attribute_name}, PhotoUploader` to your model. #{e.message}"
+          raise error.message
+        end
+        concat link_to(fa_icon(:download), src_original, class: 'btn btn-success', download: filename)
       end
-      concat link_to(fa_icon(:download), src_original, class: 'btn btn-success', download: filename)
     end
   end
 
