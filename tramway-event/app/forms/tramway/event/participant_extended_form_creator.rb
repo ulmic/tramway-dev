1b5f55354b6453ba84b081b742bb98911e6b2c7e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-14 03:04:36 +0400

    #event fix form creator

--- tramway-event/app/forms/tramway/event/participant_extended_form_creator.rb
+++ tramway-event/app/forms/tramway/event/participant_extended_form_creator.rb
@@ -4,7 +4,10 @@ class Tramway::Event::ParticipantExtendedFormCreator < Tramway::Core::FormCreato
   def self.create_form_class(uuid, event, **simple_properties)
     class_name = "ParticipantExtendedForm#{uuid.gsub('-', '')}"
     properties = event.participant_form_fields.inputs_list.reduce({}) do |hash, field|
-      hash.merge! field.title.to_sym => { object: field, validates: field.options&.dig('validations') }
+      hash.merge! field.title.to_sym => {
+        object: field,
+        validates: (field.options == '' ? {} : field.options)&.dig('validations')
+      }
     end
     if simple_properties.keys.any?
       ::Tramway::Core::ExtendableForm.new(class_name, simple_properties: simple_properties, **properties)
