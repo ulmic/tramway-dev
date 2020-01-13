104825b524402adceddc758da20fb6ba917aaf76
moshinaan <moshinahacker@yandex.ru>
2020-01-13 18:23:16 +0400

    обновлениие отображения страницы формы анкеты

--- tramway-event/app/decorators/tramway/event/participant_form_field_decorator.rb
+++ tramway-event/app/decorators/tramway/event/participant_form_field_decorator.rb
@@ -9,6 +9,10 @@ class Tramway::Event::ParticipantFormFieldDecorator < ::Tramway::Core::Applicati
     def list_attributes
       [:event_title]
     end
+
+    def show_attributes
+      [:id,:title, :description, :field_type,:event_title,:state, :created_at, :updated_at, :options, :position ]
+    end
   end
 
   def title
@@ -17,5 +21,33 @@ class Tramway::Event::ParticipantFormFieldDecorator < ::Tramway::Core::Applicati
 
   def event_title
     object.event.title
+  end
+
+  def description
+    object.description
+  end
+
+  def field_type
+    object.field_type
+  end
+
+  def state
+    object.state
+  end
+
+  def created_at
+    object.created_at
+  end
+
+  def updated_at
+    object.updated_at
+  end
+
+  def options
+    object.options
+  end
+
+  def position
+    object.position
   end
 end
