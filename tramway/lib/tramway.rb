af24eb7317e2b538fbbb456d93494ac7fcdb98c9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-20 03:10:02 +0400

    #tramway cleaned tramway directory

--- tramway/lib/tramway.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require 'tramway/version'
-
-module Tramway
-  # Your code goes here...
-end
--- tramway/lib/tramway/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Tramway
-  VERSION = '0.1.0'
-end
