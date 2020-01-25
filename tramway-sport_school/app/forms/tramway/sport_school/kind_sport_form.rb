d9e76689ed2a7a74e9c69146f73d75cd1db144f2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-01-26 03:18:03 +0400

    #sport_school some refactoring and event based state changes\

--- tramway-sport_school/app/forms/tramway/sport_school/kind_sport_form.rb
+++ tramway-sport_school/app/forms/tramway/sport_school/kind_sport_form.rb
@@ -4,8 +4,8 @@ class Tramway::SportSchool::KindSportForm < ::Tramway::Core::ApplicationForm
   properties :title, :view_state, :image, :description
 
   def initialize(object)
-    form_object = super object
-    form_properties title: :string, image: :file, description: :default
-    form_object
+    super(object).tap do
+      form_properties title: :string, image: :file, description: :default
+    end
   end
 end
--- tramway-sport_school/app/forms/tramway/sport_school/organization_form.rb
+++ tramway-sport_school/app/forms/tramway/sport_school/organization_form.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Tramway::SportSchool::OrganizationForm < ::Tramway::Core::ApplicationForm
-  properties :title, :description, :logo, :organization_type, :state, :view_state, :link
+  properties :title, :description, :logo, :organization_type, :state, :view_state_event, :link
 
   def initialize(object)
-    form_object = super object
-    form_properties title: :string,
-                    description: :default,
-                    organization_type: :default,
-                    logo: :file,
-                    link: :string
-    form_object
+    super(object).tap do
+      form_properties title: :string,
+        description: :default,
+        organization_type: :default,
+        logo: :file,
+        link: :string
+    end
   end
 end
