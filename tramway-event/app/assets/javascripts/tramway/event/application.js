3b6d933748a06da2633d11feeafa615f0f84d45b
moshinaan <moshinahacker@yandex.ru>
2020-01-26 02:50:51 +0400

    #tramway-event delete gem turbolinks

--- tramway-event/app/assets/javascripts/tramway/event/application.js
+++ tramway-event/app/assets/javascripts/tramway/event/application.js
@@ -1,13 +1,5 @@
 //= require tramway/core/application
 //= require_tree .
 //= require clipboard
-//= require turbolinks
 
 window.current_locale = window.i18n_locale('ru');
-
-$(document).ready(function(){  
-  
-  var clipboard = new Clipboard('.clipboard-btn');
-  console.log(clipboard);
-	
-});
