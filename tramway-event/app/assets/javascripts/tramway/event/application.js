ac00de99817bffe7c7ac35ee1020693d4948b120
moshinaan <moshinahacker@yandex.ru>
2020-01-26 02:25:50 +0400

    #tramway-event add gem clipboard-rails

--- tramway-event/app/assets/javascripts/tramway/event/application.js
+++ tramway-event/app/assets/javascripts/tramway/event/application.js
@@ -1,4 +1,12 @@
 //= require tramway/core/application
 //= require_tree .
+//= require clipboard
 
 window.current_locale = window.i18n_locale('ru');
+
+$(document).ready(function(){  
+  
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+	
+});
