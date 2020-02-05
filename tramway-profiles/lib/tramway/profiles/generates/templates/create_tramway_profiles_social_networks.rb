d13fb87e32a68742676613cfcbabdff948414680
moshinaan <moshinahacker@yandex.ru>
2020-02-06 00:34:59 +0400

    #profile fix type record_id to integer

--- tramway-profiles/lib/tramway/profiles/generates/templates/create_tramway_profiles_social_networks.rb
+++ tramway-profiles/lib/tramway/profiles/generates/templates/create_tramway_profiles_social_networks.rb
@@ -5,7 +5,7 @@ class CreateTramwayProfilesSocialNetworks < ActiveRecord::Migration[5.1]
     create_table :tramway_profiles_social_networks do |t|
       t.text :title
       t.text :uid
-      t.text :record_id
+      t.integer :record_id
       t.text :record_type
       t.text :network_name
       t.text :state, default: :active
