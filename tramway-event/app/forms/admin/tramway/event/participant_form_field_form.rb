4d3b907db08ebe6f478aa81b1546db23f501f8bc
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-02-17 06:59:27 +0400

    #event add boolean checkbox for presence validator

--- tramway-event/app/forms/admin/tramway/event/participant_form_field_form.rb
+++ tramway-event/app/forms/admin/tramway/event/participant_form_field_form.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::Tramway::Event::ParticipantFormFieldForm < ::Tramway::Core::ExtendedApplicationForm
-  properties :title, :description, :field_type, :options, :position, :list_field
+  properties :title, :description, :field_type, :options, :position, :list_field, :presence_field
   association :event
 
   def initialize(object)
@@ -12,6 +12,7 @@ class Admin::Tramway::Event::ParticipantFormFieldForm < ::Tramway::Core::Extende
                       field_type: :default,
                       options: :text,
                       list_field: :boolean,
+                      presence_field: :boolean,
                       position: :numeric
     end
   end
@@ -20,12 +21,17 @@ class Admin::Tramway::Event::ParticipantFormFieldForm < ::Tramway::Core::Extende
     super(params).tap do
       model.options = {} if model.options == ''
       model.options&.merge! list_field: (params[:list_field] == '1').to_s
+      model.options&.deep_merge! validations: { presence: (params[:presence_field] == '1').to_s }
       model.save
     end
   end
 
   def list_field
     model.options&.dig('list_field') == 'true'
+  end
+
+  def presence_field
+    model.options&.dig('validations', 'presence') == 'true'
   end
 
   def options
--- tramway-event/lib/tramway/event/version.rb
+++ tramway-event/lib/tramway/event/version.rb
@@ -2,6 +2,6 @@
 
 module Tramway
   module Event
-    VERSION = '1.9.29'
+    VERSION = '1.9.30'
   end
 end
