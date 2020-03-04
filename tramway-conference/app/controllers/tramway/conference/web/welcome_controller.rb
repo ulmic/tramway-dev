aa124d2eda4a0c8ad2c0a051ca78a699c391ffd8
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-03-05 02:10:52 +0400

    #conference remove main event

--- tramway-conference/app/controllers/tramway/conference/web/welcome_controller.rb
+++ tramway-conference/app/controllers/tramway/conference/web/welcome_controller.rb
@@ -7,20 +7,12 @@ class Tramway::Conference::Web::WelcomeController < ::Tramway::Conference::Appli
     @unity = ::Tramway::Conference::Unity.includes(:social_networks).first
     @blocks = ::Tramway::Landing::BlockDecorator.decorate ::Tramway::Landing::Block.on_main_page
     @news = ::Tramway::News::News.active.first(3).map { |t| ::Tramway::Landing::Cards::NewsDecorator.new t }
-    past_events = ::Tramway::Event::EventLinkDecorator.decorate ::Tramway::Event::Event.active.past.order begin_date: :desc
+    past_events = ::Tramway::Event::EventLinkDecorator.decorate ::Tramway::Event::Event.active.past.open.order begin_date: :desc
     past_events_links = Tramway::Landing::NavbarDecorator.new [{ past_events: past_events }]
-    actual_events = ::Tramway::Event::EventLinkDecorator.decorate ::Tramway::Event::Event.active.actual.order :begin_date
+    actual_events = ::Tramway::Event::EventLinkDecorator.decorate ::Tramway::Event::Event.active.actual.open.order :begin_date
     @links = actual_events + past_events_links
-    main_event = ::Tramway::Event::Event.main_event&.last
-    @close_events = ::Tramway::Event::Event.actual.map do |event|
+    @close_events = ::Tramway::Event::Event.actual.open.map do |event|
       ::Tramway::Event::EventAsPageWithButtonDecorator.decorate event 
-    end
-    if main_event.present?
-      @main_event = ::Tramway::Event::Events::Show::EventDecorator.decorate main_event
-      @sections_as_features = main_event.sections.active.order(position: :asc).map { |s| ::Tramway::Event::SectionFeatureDecorator.decorate s }
-      @sections = main_event.sections.active.order(position: :asc).map { |s| ::Tramway::Event::SectionDecorator.decorate s }
-      @participant_form = ::Tramway::Event::ParticipantExtendedFormCreator.create_form_class(request.uuid, main_event).new ::Tramway::Event::Participant.new
-      @people_as_features = @main_event.partakings.active.map { |p| ::Tramway::Event::PartakingFeatureDecorator.decorate p }
     end
     @partners = ::Tramway::Partner::Partnership.partnership_type.values.reduce({}) do |hash, partnership_type|
       hash.merge! partnership_type => (@unity.send(partnership_type.to_s.pluralize).active.map do |partner|
