34663aa8ff3bb69438831a6cf740170bc84c2cb7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-03-05 05:46:27 +0400

    #landing remove coffeescript

---
+++ tramway-landing/app/assets/javascripts/tramway/landing/application.js
@@ -0,0 +1,19 @@
+//= require tramway/core/application
+//= require_tree .
+
+$(document).ready(function() {
+  $('.header-buttons').click(function() {
+    const anchor = $(this).data('anchor');
+    $(window).scrollTop($(anchor).offset().top);
+  });
+
+  const iframe_selectors_for_video = ['iframe[src*="vk.com"]', 'iframe[src*="youtu.be"]', 'iframe[src*="youtube.com"]'];
+  for (const selector in iframe_selectors_for_video) {
+    $(selector).each(function() {
+      width = $(this).parents('div').first().width();
+      height = width / 16 * 9;
+      $(this).width(width);
+      $(this).height(height);
+    });
+  }
+});
--- tramway-landing/lib/tramway/landing/version.rb
+++ tramway-landing/lib/tramway/landing/version.rb
@@ -2,6 +2,6 @@
 
 module Tramway
   module Landing
-    VERSION = '2.0.1.1'
+    VERSION = '2.0.1.2'
   end
 end
