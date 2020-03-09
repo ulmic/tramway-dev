726ab115fb719d40896b86f28515c9a3ae4fd89a
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-03-10 02:41:58 +0400

    #core fix checkbox input_extended and added inclusion validator

--- tramway-core/app/forms/tramway/core/extendable_form.rb
+++ tramway-core/app/forms/tramway/core/extendable_form.rb
@@ -51,10 +51,19 @@ class Tramway::Core::ExtendableForm
 
               define_method "#{property[0]}=" do |value|
                 property[1][:validates].each do |pair|
-                  validator_object = "#{pair[0].camelize}Validator".constantize.new(attributes: :not_blank)
-                  if pair[1] == 'true' && !validator_object.send(:valid?, value)
-                    model.errors.add property[0],
-                      I18n.t("activerecord.errors.models.#{model.class.name.underscore}.attributes.default.#{pair[0]}", value: value)
+                  case pair[0].to_s
+                  when 'presence'
+                    validator_object = "#{pair[0].to_s.camelize}Validator".constantize.new(attributes: :not_blank)
+                    if pair[1] == 'true' && !validator_object.send(:valid?, value)
+                      model.errors.add property[0],
+                        I18n.t("activerecord.errors.models.#{model.class.name.underscore}.attributes.default.#{pair[0]}", value: value)
+                    end
+                  when 'inclusion'
+                    in_array = pair[1][:in]
+                    unless value.in? in_array
+                      model.errors.add property[0],
+                        I18n.t("activerecord.errors.models.#{model.class.name.underscore}.attributes.default.#{pair[0]}", value: value)
+                    end
                   end
                 end
               end
--- tramway-core/lib/tramway/core/version.rb
+++ tramway-core/lib/tramway/core/version.rb
@@ -2,6 +2,6 @@
 
 module Tramway
   module Core
-    VERSION = '1.17'
+    VERSION = '1.17.1'
   end
 end
