ee4f991de52cce8bdd9bf3b79acc93524a35d797
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-03-17 03:15:28 +0400

    #sport_school fix javascripts

--- tramway-sport_school/app/assets/javascripts/tramway/sport_school/application.js
+++ tramway-sport_school/app/assets/javascripts/tramway/sport_school/application.js
@@ -2,14 +2,3 @@
 //= require jquery_ujs
 //= require tramway/sport_school/jquery.scrollTo.min
 //= require_tree .
-
-$.extend($.scrollTo.defaults, {
-    axis: 'y',
-    duration: 2000
-});
-$(document).ready(function() {
-  $('.scroll').click(function() {
-    e.preventDefault();
-    $.scrollTo("#" + $(@).data('id'));
-  });
-});
