767d17eaec8e4ba9bbc91ca204fa79a9bc7f6db5
moshinaan <moshinahacker@yandex.ru>
2020-03-31 00:46:17 +0400

    Revert "fix bug"
    This reverts commit cbf9c329a70563ec8fd251bac2b559e15ec53aef.

--- tramway-event/app/models/tramway/event/event.rb
+++ tramway-event/app/models/tramway/event/event.rb
@@ -6,7 +6,7 @@ class Tramway::Event::Event < ::Tramway::Event::ApplicationRecord
   validate :check_dimensions
 
   def check_dimensions
-    if photo.present? && photo.present? && photo.height.present?
+    if photo.present?
       errors.add :photo, :too_small_image if photo.width < 1920 || photo.height < 1080
     end
   end
@@ -34,9 +34,7 @@ class Tramway::Event::Event < ::Tramway::Event::ApplicationRecord
     return :not_initialized unless request_collecting_begin_date.present? || request_collecting_end_date.present?
     return :will_begin_soon if request_collecting_begin_date > DateTime.now
     return :is_over if request_collecting_end_date.present? && request_collecting_end_date.to_date < Date.today
-    if request_collecting_begin_date&.past? && (request_collecting_end_date&.future? || request_collecting_end_date&.today?)
-      return :are_being_right_now
-    end
+    return :are_being_right_now if request_collecting_begin_date&.past? && (request_collecting_end_date&.future? || request_collecting_end_date&.today?)
     return :are_being_right_now if request_collecting_begin_date < DateTime.now && !request_collecting_end_date.present?
   end
 
