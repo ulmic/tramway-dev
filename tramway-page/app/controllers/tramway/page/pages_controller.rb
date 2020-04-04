739bd576fb81b27c5147ca449fbdfac1a3f2aa12
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-04-05 03:53:27 +0400

    #page fix publihed blocks

--- tramway-page/app/controllers/tramway/page/pages_controller.rb
+++ tramway-page/app/controllers/tramway/page/pages_controller.rb
@@ -5,7 +5,7 @@ class ::Tramway::Page::PagesController < ::Tramway::Page::ApplicationController
 
   def show
     @page = ::Tramway::Page::Page.find_by slug: params[:slug]
-    @blocks = @page.blocks.active.map do |block|
+    @blocks = @page.blocks.published.active.map do |block|
       if block.block_type.header_with_form? && block.form_url.present?
         # FIXME in future
         @header_with_form = block.form_to_render.new(Tramway::Auth.authenticable_models.first.new)
