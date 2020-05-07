fdfce6f3aa4e73856c36b5c4e7c4baa5e4a08813
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-05-08 00:00:10 +0400

    #landing fix bugs

--- tramway-landing/app/helpers/tramway/landing/application_helper.rb
+++ tramway-landing/app/helpers/tramway/landing/application_helper.rb
@@ -11,6 +11,22 @@ module Tramway
         forms = forms.select { |f| f.form_name != 'user_sign_in' } if @signed_in
         forms
       end
+      
+      def block_title(block)
+        if block.page.page_type.main?
+          yield(:application_name).present? ? yield(:application_name) : @application.public_name
+        else
+          block.page.title
+        end
+      end
+
+      def block_tagline(block)
+        if block.page.page_type.main?
+          yield(:application_tagline).present? ? yield(:application_tagline) : @application.tagline
+        else
+          raw block.page.body
+        end
+      end
     end
   end
 end
