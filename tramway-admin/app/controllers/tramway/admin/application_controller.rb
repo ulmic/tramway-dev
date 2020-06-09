7ec31736274ba5d045ffdf1f71fde18489252bda
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-06-09 05:47:08 +0400

    admin fix bug

--- tramway-admin/app/controllers/tramway/admin/application_controller.rb
+++ tramway-admin/app/controllers/tramway/admin/application_controller.rb
@@ -97,7 +97,7 @@ module Tramway
         #  model: params[:model]
         # )
         # raise "Looks like model #{params[:model]} is not included to tramway-admin for `#{current_admin.role}` role. Add it in the `config/initializers/tramway.rb`. This way `Tramway::Admin.set_available_models(#{params[:model]})`"
-        Tramway::Admin.forms.include? params[:form].underscore.sub(%r{^admin/}, '').sub(/_form$/, '')
+        Tramway::Admin.forms.include? params[:form].underscore.sub(%r{^admin/}, '').sub(/_form$/, '') if params[:form].present?
       end
 
       def available_scope_given?
