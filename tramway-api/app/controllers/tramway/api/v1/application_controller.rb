317b7db800927869d0a10b12e0b73e0e2d116686
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-07-26 22:00:54 +0400

    #api fix default apu methods

--- tramway-api/app/controllers/tramway/api/v1/application_controller.rb
+++ tramway-api/app/controllers/tramway/api/v1/application_controller.rb
@@ -25,7 +25,7 @@ module Tramway
 
         def record
           if params[:key].present?
-            if Tramway::Api.other_id_methods_of(model: model_class).include? params[:key]
+            if Tramway::Api.other_id_methods_of(model: model_class).include? params[:key].to_sym
               @record = model_class.find_by! params[:key] => params[:id] if params[:id].present?
             end
           else
