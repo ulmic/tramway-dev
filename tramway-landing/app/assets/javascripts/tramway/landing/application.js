c6239c7d13cb7121bdfafcc1a4b35fe1f9336cf9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2020-09-15 16:07:35 +0400

    #landing fix bagulya

--- tramway-landing/app/assets/javascripts/tramway/landing/application.js
+++ tramway-landing/app/assets/javascripts/tramway/landing/application.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
   });
 
   const iframe_selectors_for_video = ['iframe[src*="vk.com"]', 'iframe[src*="youtu.be"]', 'iframe[src*="youtube.com"]'];
-  for (const selector in iframe_selectors_for_video) {
+  iframe_selectors_for_video.forEach(function(selector) {
     $(selector).each(function() {
       width = $(this).parents('div').first().width();
       height = width / 16 * 9;
       $(this).width(width);
       $(this).height(height);
     });
-  }
+  })
 });
