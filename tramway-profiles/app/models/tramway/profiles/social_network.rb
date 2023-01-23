1be409177cee736f5b6709962a02a7f584d022b0
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-01-23 05:13:53 +0400

    Bug fix

--- tramway-profiles/app/models/tramway/profiles/social_network.rb
+++ tramway-profiles/app/models/tramway/profiles/social_network.rb
@@ -4,5 +4,5 @@ class Tramway::Profiles::SocialNetwork < Tramway::ApplicationRecord
   belongs_to :record, polymorphic: true, required: false
 
   enumerize :network_name, in: %i[vk facebook twitter instagram telegram patreon]
-  enumerize :record_type, in: Tramway::Prefiles.records
+  enumerize :record_type, in: Tramway::Profiles.records
 end
