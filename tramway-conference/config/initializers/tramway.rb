08a01736059a05d62e1a0c76f4fd84890750bcae
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2023-02-09 18:39:19 +0400

    Fix initializers

--- tramway-conference/config/initializers/tramway.rb
+++ tramway-conference/config/initializers/tramway.rb
@@ -3,17 +3,21 @@
 Rails.application.config.after_initialize do
   Tramway.root_path_for Tramway::User => '/admin'
   Tramway.set_singleton_models Tramway::Conference::Unity, project: :conference
-  Tramway.set_available_models(Tramway::Event::Event,
-                               Tramway::Event::ParticipantFormField,
-                               Tramway::Event::Participant,
-                               Tramway::Event::Section,
-                               Tramway::Event::Person,
-                               Tramway::Event::Partaking,
-                               Tramway::Event::Place,
-                               Tramway::Event::Action,
-                               Tramway::User,
-                               Tramway::Profiles::SocialNetwork,
-                               project: :conference)
+  Tramway.set_available_models(
+    [
+      Tramway::Event::Event,
+      Tramway::Event::ParticipantFormField,
+      Tramway::Event::Participant,
+      Tramway::Event::Section,
+      Tramway::Event::Person,
+      Tramway::Event::Partaking,
+      Tramway::Event::Place,
+      Tramway::Event::Action,
+      Tramway::User,
+      Tramway::Profiles::SocialNetwork
+    ],
+    project: :conference
+  )
   Tramway.initialize_application model_class: Tramway::Conference::Unity
   Tramway::Landing.head_content = lambda do
     concat stylesheet_link_tag    'tramway/conference/application', media: 'all'
--- tramway-profiles/config/initializers/tramway.rb
+++ tramway-profiles/config/initializers/tramway.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
-  Tramway.set_available_models(Tramway::Profiles::SocialNetwork, project: :profiles)
+  Tramway.set_available_models(
+    [
+      Tramway::Profiles::SocialNetwork
+    ], project: :profiles
+  )
 end
